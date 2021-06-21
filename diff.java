ecab92c80bd07220b217b84b22c0d0d235bc1f87
ritik joshi <joshiritik895@gmail.com>
2021-06-21 18:30:27 +0530

    diff functionality added in existing

--- diff.java
+++ diff.java
@@ -1,3 +1,12 @@
 public class diff{
     
+    public static void main(Sting args[]){
+        
+        int a = 1;
+        int b = 1;
+
+        System.out.println("diff is = " + (a-b));
+        
+    }
+
 }
